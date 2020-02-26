dc106c1a53c2f1142481d59fbf2bda0a0e87bdab
Developer2 <developer2@truesys.co.in>
2020-02-26 23:25:51 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+    return (
+        <View style={styles.container} >
+            <text> sdfsfsfsfsdfsf </text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    header: {
+        backgroundColor: 'pink',
+        padding: 20,
+    },
+});C
